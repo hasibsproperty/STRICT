ff2d2bb0aa7d045c8f24f8bc650532428247161f
Hasibul Hasan <94183704+hasibsproperty@users.noreply.github.com>
2022-01-25 03:18:44 +0600

    Add files via upload

---
+++ JS/custom.js
@@ -0,0 +1,10 @@
+let fname = document.getElementById("fname")
+let fname_err = document.getElementById("fname_err")
+
+function sub() {
+
+    if (fname.value == "") {
+        console.log("")
+        return false
+    }
+}
